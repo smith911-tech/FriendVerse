35466c9b639cdb4f750da47d8453acddb1c52b98
joseph williams <wj882244@gmail.com>
2023-07-10 09:31:55 +0100

    redesigned 404 page

--- src/Home Comp/HomePage.tsx
+++ src/Home Comp/HomePage.tsx
@@ -18,18 +18,3 @@ export default function HomePage() {
         </h2>
     )
 }
-
-// import React from 'react';
-// import { auth } from './firebase';
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import Login from './login';
-// import Mainpage from './main';
-
-// function App() {
-//     const [user] = useAuthState(auth);
-//     return (
-//         user ? <Mainpage /> : <Login />
-//     );
-// }
-
-// export default App;
--- src/Page404.tsx
+++ src/Page404.tsx
@@ -1,6 +1,13 @@
+import BrokenPencil from './assets/Broken Pencil.png'
+import { useNavigate } from 'react-router-dom'
 export default function Page404(){
+  const navigate = useNavigate()
+  const handlenavigate = () =>{
+    navigate('/Homepage')
+  }
     return(
-    <div className="flex items-center justify-center min-h-screen bg-black">
+      <div className="flex  justify-center min-h-screen bg-black flex-col ">
+        <img src={BrokenPencil} alt="" className='w-full mt-[-252px] object-contain sm500:w-[350px] block mb-0 mx-auto'/>
       <div className="text-white text-center">
         <h1 className="text-6xl font-bold mb-8 text-[#117DD5]" >
           404
@@ -8,6 +15,8 @@ export default function Page404(){
         <p className="text-2xl">
           Oops! The page you're looking for doesn't exist.
         </p>
+        <br />
+          <button onClick={handlenavigate} className='py-2 bg-[#0145d4] font-bold px-4 rounded-2xl'>Go To Website</button>
       </div>
     </div>
     )
