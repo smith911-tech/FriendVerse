b38b441c2093947a420b965e2295f2134b5417a0
joseph williams <wj882244@gmail.com>
2023-07-13 12:32:03 +0100

    changed the preloader

--- src/Preloader.tsx
+++ src/Preloader.tsx
@@ -1,8 +1,8 @@
 import logo from '../src/assets/Logo.png'
 export default function Preloader() {
   return (
-        <div className='bg-[#1B1D21] h-screen preloader'>
-          <img src={logo} alt="Logo" className="preloader-spinner" />
+        <div className='bg-[#1B1D21] h-screenr preloader'>
+      <img src={logo} alt="Logo" className="preloader-logo" loading='lazy'/>
         </div>
   );
 }        
