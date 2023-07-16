9b32ad69aec20a60a50293f9986d3d7fc1de8c99
joseph williams <wj882244@gmail.com>
2023-07-16 09:32:28 +0100

    resetting

--- src/Home Comp/Dashboard.tsx
+++ src/Home Comp/Dashboard.tsx
@@ -1,6 +1,8 @@
+import logo from '../assets/Logo2.png'
 export default function Dashboard() {
     return (   
         <main className="">
+            <img src={logo} alt="logo" className='w- w-14'/>
         </main>
     )
 }
--- src/Home Comp/HomePage.tsx
+++ src/Home Comp/HomePage.tsx
@@ -8,6 +8,7 @@ export default function HomePage() {
     const [Toggle, setToggle] = useState<boolean>(false)
     const handleToggle = () => {
         setToggle(!Toggle)
+        handleToggle()
     }
     const navigate = useNavigate();
     useEffect(() => {
@@ -20,21 +21,18 @@ export default function HomePage() {
         }
     }, [])
     return (
-        <main className="bg-white flex justify-between gap-[5%]">
+        <main className="bg-[#f0f2f5] flex justify-between gap-[5%]">
             <section
                 className="bg-white w-[30%]"
             >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis corrupti obcaecati incidunt sed debitis saepe maxime tempora beatae aperiam quas hic, voluptatem recusandae animi magnam iure ad. Voluptate accusantium commodi necessitatibus laudantium maiores nobis ratione, voluptas earum id cum, quibusdam ipsum vel dignissimos ullam magni alias optio expedita explicabo eum pariatur. Exercitationem, perferendis ad. Ipsum officiis consectetur repellat excepturi aut blanditiis qui cum magni error, eos asperiores similique sunt saepe ex repellendus nihil? Iusto reprehenderit temporibus magni earum provident suscipit adipisci vel voluptatum eum? Soluta aspernatur atque, nobis, dignissimos modi culpa optio exercitationem voluptas libero at vero? Itaque quam iste iusto! Reprehenderit ullam consequatur, quia voluptas, nam dolorem dignissimos natus quam nemo cum numquam neque autem sunt praesentium et vero id sequi at quaerat ex consequuntur reiciendis. Fugiat, alias fuga! Magnam tempora necessitatibus quos quisquam. Consectetur dolorum nesciunt repellendus maxime nostrum odit maiores a et voluptas facilis magnam neque velit totam repudiandae non adipisci, molestiae suscipit fugit accusamus dicta provident praesentium perferendis veniam? Odit provident quasi sed voluptates quo reprehenderit, ipsam, neque repellat, ducimus sunt iusto soluta. Facere doloremque accusantium rem magnam libero aliquam enim illum! Placeat, est! Nostrum vero eaque laborum molestias illo eveniet quae iusto unde, consequatur rem adipisci quisquam ab, sapiente optio ipsum enim fuga exercitationem commodi animi perferendis vel modi pariatur! Magni, atque? Quae eum enim eos consequuntur!
                 <Dashboard />
             </section>
             <section
                 className="bg-white w-[35%]">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis corrupti obcaecati incidunt sed debitis saepe maxime tempora beatae aperiam quas hic, voluptatem recusandae animi magnam iure ad. Voluptate accusantium commodi necessitatibus laudantium maiores nobis ratione, voluptas earum id cum, quibusdam ipsum vel dignissimos ullam magni alias optio expedita explicabo eum pariatur. Exercitationem, perferendis ad. Ipsum officiis consectetur repellat excepturi aut blanditiis qui cum magni error, eos asperiores similique sunt saepe ex repellendus nihil? Iusto reprehenderit temporibus magni earum provident suscipit adipisci vel voluptatum eum? Soluta aspernatur atque, nobis, dignissimos modi culpa optio exercitationem voluptas libero at vero? Itaque quam iste iusto! Reprehenderit ullam consequatur, quia voluptas, nam dolorem dignissimos natus quam nemo cum numquam neque autem sunt praesentium et vero id sequi at quaerat ex consequuntur reiciendis. Fugiat, alias fuga! Magnam tempora necessitatibus quos quisquam. Consectetur dolorum nesciunt repellendus maxime nostrum odit maiores a et voluptas facilis magnam neque velit totam repudiandae non adipisci, molestiae suscipit fugit accusamus dicta provident praesentium perferendis veniam? Odit provident quasi sed voluptates quo reprehenderit, ipsam, neque repellat, ducimus sunt iusto soluta. Facere doloremque accusantium rem magnam libero aliquam enim illum! Placeat, est! Nostrum vero eaque laborum molestias illo eveniet quae iusto unde, consequatur rem adipisci quisquam ab, sapiente optio ipsum enim fuga exercitationem commodi animi perferendis vel modi pariatur! Magni, atque? Quae eum enim eos consequuntur!
                 <Content />
             </section>
             <section
                 className="bg-white  w-[30%]">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis corrupti obcaecati incidunt sed debitis saepe maxime tempora beatae aperiam quas hic, voluptatem recusandae animi magnam iure ad. Voluptate accusantium commodi necessitatibus laudantium maiores nobis ratione, voluptas earum id cum, quibusdam ipsum vel dignissimos ullam magni alias optio expedita explicabo eum pariatur. Exercitationem, perferendis ad. Ipsum officiis consectetur repellat excepturi aut blanditiis qui cum magni error, eos asperiores similique sunt saepe ex repellendus nihil? Iusto reprehenderit temporibus magni earum provident suscipit adipisci vel voluptatum eum? Soluta aspernatur atque, nobis, dignissimos modi culpa optio exercitationem voluptas libero at vero? Itaque quam iste iusto! Reprehenderit ullam consequatur, quia voluptas, nam dolorem dignissimos natus quam nemo cum numquam neque autem sunt praesentium et vero id sequi at quaerat ex consequuntur reiciendis. Fugiat, alias fuga! Magnam tempora necessitatibus quos quisquam. Consectetur dolorum nesciunt repellendus maxime nostrum odit maiores a et voluptas facilis magnam neque velit totam repudiandae non adipisci, molestiae suscipit fugit accusamus dicta provident praesentium perferendis veniam? Odit provident quasi sed voluptates quo reprehenderit, ipsam, neque repellat, ducimus sunt iusto soluta. Facere doloremque accusantium rem magnam libero aliquam enim illum! Placeat, est! Nostrum vero eaque laborum molestias illo eveniet quae iusto unde, consequatur rem adipisci quisquam ab, sapiente optio ipsum enim fuga exercitationem commodi animi perferendis vel modi pariatur! Magni, atque? Quae eum enim eos consequuntur!
                 <SideDashboard />
             </section>
         </main>
