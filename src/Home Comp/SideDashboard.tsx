567cbf514c23d4c5e1ce76d41ea3416d6fa42e4b
joseph williams <wj882244@gmail.com>
2023-07-17 11:06:13 +0100

    finished the sidebar

--- src/Home Comp/SideDashboard.tsx
+++ src/Home Comp/SideDashboard.tsx
@@ -4,6 +4,7 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from '../firebase-config'
 import { GoAlertFill } from "react-icons/go";
 import { BiSolidUserCircle } from "react-icons/bi";
+import VerfiyId from "../Verify";
 export default function SideDashboard() {
     const [SuggestData, setSuggestData] = useState<any[]>([]);
     const [searchTerm, setSearchTerm] = useState<string>("");
@@ -28,7 +29,7 @@ export default function SideDashboard() {
     }
     return (
         <main className="lg1150:block hidden px-1 pt-10 ">
-            <section className="bg-[white]  px-2 py-2 shadow-2xl relative">
+            <section className="bg-[white]  px-2 py-2 shadow-2xl relative mb-14">
                 <div className="flex relative">
                     <input type="text"
                         className="w-full py-2 pl-10 pr-1  outline-[#117DD5] border rounded-2xl  border-solid bg-[#eff3f4]" placeholder="Search"
@@ -73,6 +74,7 @@ export default function SideDashboard() {
                     </div>
                 )}
             </section>
+            <VerfiyId />
         </main>
     )
 }
---
+++ src/Verify.tsx
@@ -0,0 +1,13 @@
+
+export default function VerfiyId() {
+    return(
+        <div className="bg-blue-200 rounded-lg shadow-lg p-4">
+            <h2 className="text-2xl font-bold mb-2">Get Verified</h2>
+            <p className="text-base text-gray-800">Verify Account to get a blue tick</p>
+            <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded mt-4">
+                Get Verified
+            </button>
+        </div>
+
+    )
+}
