477414969075a58669007612006a76b1af26368e
joseph williams <wj882244@gmail.com>
2023-07-19 14:04:03 +0100

    creating content page

--- src/Home Comp/PostSection.tsx
+++ src/Home Comp/PostSection.tsx
@@ -1,18 +1,18 @@
-interface userdatas{
-    handleBodyClick : () => void,
+interface userdatas {
+    handleBodyClick: () => void,
     handleInputClick: () => void,
     isInputClicked: boolean,
-    userData : any
+    userData: any
 }
-import { FaXmark  } from "react-icons/fa6";
+import { FaXmark } from "react-icons/fa6";
 import { BiSolidUserCircle } from 'react-icons/bi';
 export default function PostSection({
     handleBodyClick,
-    userData, 
+    userData,
     isInputClicked,
-    handleInputClick}: userdatas): JSX.Element{
+    handleInputClick }: userdatas): JSX.Element {
     const firstName = userData?.fullName?.split(' ')[0] ?? 'Loading....';
-    return(
+    return (
         <>
             <header className="bg-white mb-2 py-2 px-5 rounded-2xl shadow md970:w-[90%] block mt-0 mx-auto ">
                 <nav className="flex justify-between gap-2">
@@ -50,13 +50,31 @@ export default function PostSection({
 
             {isInputClicked && (
                 <div className="absolute top-0 left-0 right-0 mx-auto bg-white p-4 z-50 rounded-2xl shadow md970:w-[90%] ">
-                    <section className="flex justify-between">
+                    <section className="flex justify-between mb-1">
                         <h2 className='text-xl font-bold'>Create Post</h2>
                         <span className="text-xl bg-[#f0f2f5] mb-2 rounded-full text-[#0000009b] px-1 py-1 cursor-pointer" onClick={handleBodyClick}>
                             <FaXmark />
                         </span>
                     </section>
                     <hr />
+                    <section className="flex gap-2">
+                        <div>
+                            {userData && userData.profileImage === "" ? (
+                                <div className='text-[48px] rounded-full text-[#000000d7]'>
+                                    <BiSolidUserCircle />
+                                </div>
+                            ) : (
+                                <img
+                                    src={userData.profileImage}
+                                    alt="Profile"
+                                    className="w-12 h-12 rounded-full object-contain"
+                                />
+                            )}
+                        </div>
+                        <h2 className=" text-lg mt-3">
+                            {userData && userData.fullName}
+                        </h2>
+                    </section>
                 </div>
             )}
         </>
