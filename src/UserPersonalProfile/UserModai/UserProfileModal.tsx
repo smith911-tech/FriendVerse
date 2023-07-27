f234021720a8040a90b092685c4eb016bd9b8046
joseph williams <wj882244@gmail.com>
2023-07-27 09:15:15 +0100

    update

--- src/UserPersonalProfile/UserModai/UserProfileModal.tsx
+++ src/UserPersonalProfile/UserModai/UserProfileModal.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineClose } from 'react-icons/ai'
 import defaultcoverimg from '../../assets/DefalutCoverImg.jpg'
+import {useEffect} from 'react'
 
 
 interface userdatas{
@@ -9,6 +10,15 @@ interface userdatas{
 
 }
 export function UserCoverImg({ userData, handleCloseModal, showCmodal }: userdatas) {
+    useEffect(() => {
+        if (showCmodal) {
+            // Add the 'overflow-hidden' class to the body when the modal is shown
+            document.body.classList.add('overflow-hidden');
+        } else {
+            // Remove the 'overflow-hidden' class from the body when the modal is hidden
+            document.body.classList.remove('overflow-hidden');
+        }
+    }, [showCmodal]);
     return(
     <>
             {showCmodal && (
@@ -16,7 +26,7 @@ export function UserCoverImg({ userData, handleCloseModal, showCmodal }: userdat
                     onClick={handleCloseModal}
                     className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
                 >
-                    <div className="fixed w-full top-20 z-[40] left-0" onClick={(e) => e.stopPropagation()}>
+                    <div className="fixed w-full top-20 z-[40] left-0 select-none" onClick={(e) => e.stopPropagation()}>
                         {userData.coverImage === "" ? (
                             <img
                                 src={defaultcoverimg}
@@ -48,6 +58,15 @@ interface userdatas2 {
 
 }
 export function UserProfileImg({showPmodal, handleCloseModal, userData}: userdatas2){
+    useEffect(() => {
+        if (showPmodal) {
+            // Add the 'overflow-hidden' class to the body when the modal is shown
+            document.body.classList.add('overflow-hidden');
+        } else {
+            // Remove the 'overflow-hidden' class from the body when the modal is hidden
+            document.body.classList.remove('overflow-hidden');
+        }
+    }, [showPmodal]);
     return(
         <>
             {showPmodal && (
@@ -55,13 +74,13 @@ export function UserProfileImg({showPmodal, handleCloseModal, userData}: userdat
                     onClick={handleCloseModal}
                     className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
                 >
-                    <div className="flex items-center">
+                    <div className="flex items-center select-none">
                         <img
                             src={userData.profileImage}
                             alt="Profile"
-                            className="w-44 h-44 rounded-full object-cover bg-white sm:h-20 sm:w-20"
+                            className="w-60 h-60 rounded-full object-cover bg-white sm:h-20 sm:w-20"
                         />
-                        <span className="absolute -top-10 right-[10%] text-white text-[40px] cursor-pointer">
+                        <span className="absolute top-10 right-[10%] text-white text-[40px] cursor-pointer ">
                             <AiOutlineClose onClick={handleCloseModal} />
                         </span>
                     </div>
