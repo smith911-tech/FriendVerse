4daaff29a12b39c434a43ceb75378acadb54c5cb
Williams Joseph <wj882244@gmail.com>
2023-08-02 01:25:31 +0100

    Creating settings page

--- src/Settings/SettingsInterface.tsx
+++ src/Settings/SettingsInterface.tsx
@@ -6,6 +6,7 @@ import {  BsFillArrowLeftCircleFill } from 'react-icons/bs'
 import { BiSolidUserCircle } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 import { LongCard } from '../GeneralComponent/LoadingCard'
+import DeleteModal from './DeleteModal'
 
 export default function SettingsInterface({userData}: userdatas){
     const [showDeleteModal, setShowDeleteModal] = useState(false);
@@ -106,25 +107,7 @@ export default function SettingsInterface({userData}: userdatas){
                         </div>
                     </section>
                     {showDeleteModal && (
-                        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                            <div className="bg-white p-4 rounded-lg w-[400px]">
-                                <h2 className="text-xl font-bold mb-4">Confirm Account Deletion</h2>
-                                <p className="mb-4">Are you sure you want to delete your account?</p>
-                                <div className="flex justify-between">
-                                    <button
-                                        className="bg-red-500 text-white px-4 py-2 rounded-lg"
-                                    >
-                                        Yes, Delete
-                                    </button>
-                                    <button
-                                        className="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg"
-                                        onClick={() => setShowDeleteModal(false)}
-                                    >
-                                        Cancel
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+                        <DeleteModal setShowDeleteModal={setShowDeleteModal}/>
                     )}
                 </main>
         )
