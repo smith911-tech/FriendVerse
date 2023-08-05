a89fec1c26ea15c95856cdfc431b06314c0d9750
Williams Joseph <wj882244@gmail.com>
2023-08-05 17:25:04 +0100

    Creating the Thememode

--- src/Usetheme.tsx
+++ src/Usetheme.tsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react';
 export function UseTheme() {
-    const [theme, setTheme] = useState(localStorage.getItem('ThemeMode') === 'true');
+    const [theme, setTheme] = useState(() => {
+        const storedTheme = localStorage.getItem('ThemeMode');
+        return storedTheme === 'true';
+    });
+
     const handleChangeTheme = () => {
         setTheme((prevTheme) => !prevTheme);
     };
+
     useEffect(() => {
         localStorage.setItem('ThemeMode', String(theme));
     }, [theme]);
+
     return { theme, handleChangeTheme };
 };
