843c8ea6f56c8a888f1012b7f0463957009d0fb8
Williams Joseph <wj882244@gmail.com>
2023-08-14 13:00:04 +0100

    creating the interface for others profile followers/following interface

--- src/UserPersonalProfile/Following/Following.tsx
+++ src/UserPersonalProfile/Following/Following.tsx
@@ -1,8 +1,11 @@
 import FollowersFollowingH from '../FollowingFollowersH'
+import { useThemeStore } from '../../Zustand';
 
 export default function Following() {
+    const theme = useThemeStore((state: any) => state.theme);
     return (
-        <main className='min-h-screen bg-white'>
+        <main className={`min-h-screen 
+        ${theme ? "bg-black text-white" : "bg-white text-black"}`}>
             <FollowersFollowingH />
         </main>
     )
--- src/UserPersonalProfile/FollowingFollowersH.tsx
+++ src/UserPersonalProfile/FollowingFollowersH.tsx
@@ -1,29 +1,36 @@
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { Link, NavLink } from 'react-router-dom'
+import { useThemeStore } from '../Zustand';
 
 export default function FollowersFollowingH(){
+    const theme = useThemeStore((state: any) => state.theme);
     const username = sessionStorage.getItem('username')
     const name = sessionStorage.getItem('name')
     return(
-        <header className="py-3">
+        <header className="py-3 select-none">
             <section className='flex px-2 gap-5'>
-                <Link to='/Profile' className='text-2xl mt-2 p-2 rounded-full hover:bg-[#00000050] transition-all cursor-pointer'>
+                <Link to='/Profile' className={`text-2xl m-2 p-2 rounded-full transition-all cursor-pointer 
+                ${theme ? "hover:bg-[#ffffff7d] " : "hover:bg-[#00000050] "}`}>
                     <AiOutlineArrowLeft/>
                 </Link>
                 <div>
                     <p className=' font-semibold text-lg'
                     >{name}</p>
-                    <p className='font-semibold text-[#000000bf]'
+                    <p 
+                    className={`font-semibold 
+                    ${theme ? "text-[#ffffffa0]" : "text-[#000000bf]"}`}
                     >@{username}</p>
                 </div>
             </section>
             <section className='flex gap-2 px-2 mb-1'>
-                <Link to='/Followers' className='w-1/2 mt-2  py-3 hover:bg-[#0000004e] text-center font-semibold transition-all'>
+                <Link to='/Followers' className={`w-1/2 mt-2  py-3 text-center font-semibold transition-all 
+                ${theme ? "hover:bg-[#ffffff5b]" : " hover:bg-[#0000004e]"}`}>
                     <NavLink to='/Followers' className='ProfileActiveF pb-1'>
                         Followers
                     </NavLink>
                 </Link>
-                <Link to='/Following' className='w-1/2 mt-2  py-3 hover:bg-[#0000004e] text-center font-semibold transition-all'>
+                <Link to='/Following' className={`w-1/2 mt-2  py-3 text-center font-semibold transition-all 
+                ${theme ? "hover:bg-[#ffffff5b]" : " hover:bg-[#0000004e]"}`}>
                     <NavLink to='/Following' className='ProfileActiveF pb-1'>
                         Following
                     </NavLink>
