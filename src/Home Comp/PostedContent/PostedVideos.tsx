c91989572b58a65c727454ae829f5c8acb779c2a
Williams Joseph <wj882244@gmail.com>
2023-09-26 00:33:50 +0100

    fixed video tag poster images

--- src/Home Comp/PostedContent/PostedVideos.tsx
+++ src/Home Comp/PostedContent/PostedVideos.tsx
@@ -2,6 +2,7 @@ interface Props {
     post: any
 }
 import { useState } from "react"
+import { useThemeStore } from '../..//Zustand'
 
 export default function PostedVideo({ post }: Props) {
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
@@ -21,11 +22,15 @@ export default function PostedVideo({ post }: Props) {
             setIsPlaying(!isPlaying);
         }
     };
+    const theme = useThemeStore((state: any) => state.theme);
     return (
         <main>
             <article className=" py-2">
                 <div onClick={handleClick}>
-                <video controls className="w-full h-80" id="videoPlayer" preload="none" poster="https://yt3.ggpht.com/a/AATXAJypFMgOMCnYeUOFbdJ5vS3aWAPCfOLml0j07w=s900-c-k-c0xffffffff-no-rj-mo">
+                <video controls className="w-full h-80 cursor-pointer" id="videoPlayer" preload="none" 
+                        poster={theme 
+                        ? "https://yt3.ggpht.com/a/AATXAJypFMgOMCnYeUOFbdJ5vS3aWAPCfOLml0j07w=s900-c-k-c0xffffffff-no-rj-mo" 
+                        : "https://cdn.pixabay.com/photo/2016/11/18/11/17/youtube-1834016_1280.png"}>
                     <source src={post.video} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
