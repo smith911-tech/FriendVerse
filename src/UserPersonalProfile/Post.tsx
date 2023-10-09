5887c2cd3c79a074ea5c097a3b37391fc3abca09
Williams Joseph <wj882244@gmail.com>
2023-10-09 14:09:02 +0100

    started implementing display post in profile

--- src/UserPersonalProfile/Post.tsx
+++ src/UserPersonalProfile/Post.tsx
@@ -7,6 +7,7 @@ import { db } from '../firebase-config';
 import { useState, useEffect } from 'react'
 import { RotatingLines } from "react-loader-spinner";
 import PostNotAvaliable from '../GeneralComponent/PostNotAvailable';
+import ViewPostOnProfile from './ViewPostOnProfile';
 export default function Post({ SuggestData }: Props) {
     const [postData, setPostData] = useState<any[]>([])
     //? uid
@@ -28,32 +29,35 @@ export default function Post({ SuggestData }: Props) {
         };
     }, []);
     const PostDataByTime = postData.sort((a, b) => b.time - a.time);
-    const UserData = SuggestData.find((suggestion: any) => suggestion.id === userid);
+    const PersonalData = SuggestData.find((suggestion: any) => suggestion.id === userid);
     
     return (
         <main className={` mt-3 
         ${theme ? "bg-[#1b1d21] text-[#ffff]" : "bg-[#f0f2f5]  text-[#000000]"}`}>
             {isLoading ? (
-                <div className='flex items-center justify-center gap-2 py-5'>
-                    <RotatingLines
-                        strokeColor="grey"
-                        strokeWidth="5"
-                        animationDuration="0.75"
-                        width="50"
-                        visible={true}
-                    />
-                    <p className='text-lg text-gray-600'>Loading Post...</p>
-                </div>
+                <section className={`py-10 ${theme ? " bg-black" : "bg-white"}`}>
+                    <div className='flex items-center justify-center gap-2 py-5'>
+                        <RotatingLines
+                            strokeColor="grey"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="50"
+                            visible={true}
+                        />
+                        <p className='text-lg'>Loading Post...</p>
+                    </div>
+                </section>
             ): (
                 <section>
-                    {PostDataByTime ? (
+                    {PostDataByTime.length === 0 ? (
                         <div className={` pt-10 pb-28 ${theme ? " bg-black" : "bg-white"}`}>
                             <PostNotAvaliable />
                         </div>
                     ): (
-                        <section>
-                            
-                        </section>
+                        <ViewPostOnProfile 
+                        PersonalData={PersonalData}
+                        PostDataByTime={PostDataByTime}
+                        />
                     )}
                 </section>
             )}
---
+++ src/UserPersonalProfile/ViewPostOnProfile.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    PersonalData: any
+    PostDataByTime: any
+}
+
+export default function ViewPostOnProfile({ PersonalData, PostDataByTime }: Props){
+    return(
+        <>
+        </>
+    )
+}
