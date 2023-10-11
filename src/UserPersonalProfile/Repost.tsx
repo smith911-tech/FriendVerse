4e63e87f3fc4f1c90afb90f1ef049e503315b185
Williams Joseph <wj882244@gmail.com>
2023-10-11 08:37:14 +0100

    started implementing repost display feature

--- src/UserPersonalProfile/Repost.tsx
+++ src/UserPersonalProfile/Repost.tsx
@@ -1,6 +1,28 @@
 import { useThemeStore } from '../Zustand';
+import { collection, onSnapshot } from "firebase/firestore"
+import { db } from '../firebase-config';
+import { useState, useEffect } from 'react'
 export default function RePost() {
     const theme = useThemeStore((state: any) => state.theme);
+    const [repostData, setRePostData] = useState<any[]>([])
+    //? uid
+    let userid = sessionStorage.getItem('UserId')
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    useEffect(() => {
+        const handleGetPost = (snapshot: any) => {
+            const data = snapshot.docs
+                .map((doc: any) => ({ ...doc.data(), id: doc.id }))
+                .filter((repost: any) => repost.RepostAuthor === userid as string); // Filter rePosts by userid
+            setIsLoading(false);
+            setRePostData(data);
+        };
+        const unsubscribePost = onSnapshot(collection(db, "Repost",), handleGetPost);
+        return () => {
+            unsubscribePost()
+        };
+    }, []);
+    console.log(isLoading, repostData);
+    
     return (
         <main className={` mt-3 ${theme ? "bg-[#1b1d21] text-[#ffff]" : "bg-[#f0f2f5]  text-[#000000]"}`}>
             Reverb Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum saepe molestiae consectetur quia accusantium totam corporis earum quisquam minus, porro aliquam nesciunt vel incidunt soluta natus nostrum, perspiciatis amet quod voluptates quae rerum a itaque commodi. Molestiae aspernatur possimus amet culpa quidem, rerum delectus ipsa? Voluptatum, consequatur sunt doloribus nostrum consequuntur iusto ducimus, aperiam id reiciendis ipsam dolore itaque. A, odit, aperiam facilis repellendus, dolor aliquid consequuntur excepturi asperiores cum praesentium laboriosam voluptatibus! Libero nostrum, molestias iusto magnam delectus ex quae obcaecati quas maxime accusamus quo, sequi aut dicta voluptate? Quas eos facilis consectetur, laudantium facere ut suscipit harum nesciunt.
