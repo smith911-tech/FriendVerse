e6e4843cc71141ede0ac2e4cd68a13d7d86cfe95
Williams Joseph <wj882244@gmail.com>
2023-10-21 12:34:50 +0100

    Added under construction page

---
+++ src/GeneralComponent/UnderConstruction.tsx
@@ -0,0 +1,11 @@
+import { VscTools } from 'react-icons/vsc'
+export default function UnderConstruction(){
+    return (
+      <main>
+        <div className='flex items-center flex-col'>
+          <VscTools className=' text-9xl'/>
+          <h2 className=' font-medium'>Under Construction</h2>
+        </div>
+      </main>
+    );
+}
