590ca1c9f47f80630a5bcea1ab6bcda9e49e6252
Williams Joseph <wj882244@gmail.com>
2023-11-11 11:50:33 +0100

    Started Implementing space feature

--- src/Space/SpaceSection.tsx
+++ src/Space/SpaceSection.tsx
@@ -2,8 +2,23 @@ import UnderConstruction from "../GeneralComponent/UnderConstruction";
 import { useThemeStore } from "../Zustand";
 import { BsFillMicFill } from "react-icons/bs";
 import {AiOutlinePlus} from 'react-icons/ai'
+import { useState } from "react";
+import CreateSpace from "./CreateSpaceModal";
+
 export default function SpaceSection() {
-      const theme = useThemeStore((state: any) => state.theme);
+  const theme = useThemeStore((state: any) => state.theme);
+     const [showPopUp, setShowPopUp] = useState(false);
+     const [isMinimizing, setIsMinimizing] = useState(false);
+     const handleOpen = () => {
+       setShowPopUp(true);
+     }
+     const handleClose = () => {
+       setIsMinimizing(true);
+       setTimeout(() => {
+         setShowPopUp(false);
+         setIsMinimizing(false);
+       }, 500);
+     };
   return (
     <main
       className={` ${
@@ -16,10 +31,21 @@ export default function SpaceSection() {
         }`}
       >
         <UnderConstruction />
-        <div className="fixed bottom-8 lg1150:right-[25%] md970:right-[13%] md800:right-[22%] sm500:right-[10%] smm500:right-[10%] sm650:bottom-24 z-50 text-2xl bg-[#3b82f6] p-3 rounded-full cursor-pointer text-white">
+        <button
+          onClick={handleOpen}
+          className="fixed bottom-8 lg1150:right-[25%] md970:right-[13%] md800:right-[22%] sm500:right-[10%] smm500:right-[10%] sm650:bottom-24 z-50 text-2xl bg-[#3b82f6] p-3 rounded-full cursor-pointer text-white"
+        >
           <BsFillMicFill className="w-auto " />
           <AiOutlinePlus className=" absolute top-2 right-2 text-xs" />
-        </div>
+        </button>
+        <section className=" relative">
+          {showPopUp && (
+            <CreateSpace
+              handleClose={handleClose}
+              isMinimizing={isMinimizing}
+            />
+          )}
+        </section>
       </div>
     </main>
   );
